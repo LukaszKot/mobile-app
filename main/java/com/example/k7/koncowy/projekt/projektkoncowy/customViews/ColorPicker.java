26c5247aa4aeab3230fa62f142f4ce2770e33e36
a <a>
2019-04-18 12:43:50 +0200

    added ColorPicker view.

--- main/java/com/example/k7/koncowy/projekt/projektkoncowy/customViews/ColorPicker.java
+++ main/java/com/example/k7/koncowy/projekt/projektkoncowy/customViews/ColorPicker.java
@@ -1,10 +1,13 @@
 package com.example.k7.koncowy.projekt.projektkoncowy.customViews;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AlphaAnimation;
+import android.view.ViewParent;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -24,7 +27,9 @@ public class ColorPicker extends RelativeLayout {
             }
         });
         addPreviewLayout();
-
+        addPattern();
+        addAccept();
+        addDecline();
     }
 
     private void addPreviewLayout()
@@ -33,9 +38,57 @@ public class ColorPicker extends RelativeLayout {
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 200);
-        //params.addRule(;);
-        //preview.setLayoutParams();
+        preview.setLayoutParams(params);
         preview.setBackgroundColor(Color.parseColor("#ff00ff"));
         this.addView(preview);
+    }
+
+    @SuppressLint("ResourceType")
+    private void addPattern()
+    {
+        pattern = new ImageView(getContext());
+        pattern.setId(156);
+        pattern.setImageDrawable(getImageFromResource("@drawable/rgb_pattern"));
+        pattern.setLayoutParams(new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        this.addView(pattern);
+    }
+
+    private void addAccept()
+    {
+        accept = new ImageView(getContext());
+        accept.setImageDrawable(getImageFromResource("@drawable/baseline_done_black_48dp"));
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(150,150);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        accept.setLayoutParams(params);
+        accept.setBackgroundColor(0xffffffff);
+        this.addView(accept);
+    }
+
+    private void addDecline() {
+        decline = new ImageView(getContext());
+        decline.setImageDrawable(getImageFromResource("@drawable/baseline_clear_black_48dp"));
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(150,150);
+        params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        decline.setLayoutParams(params);
+        decline.setBackgroundColor(0xffffffff);
+        decline.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View theView = (View)decline.getParent();
+                ((ViewGroup)theView.getParent()).removeView(theView);
+            }
+        });
+        this.addView(decline);
+    }
+
+    private Drawable getImageFromResource(String name)
+    {
+        int imageResource = getContext().getResources()
+                .getIdentifier(name, null,
+                        getContext().getPackageName());
+        Drawable res = getContext().getResources().getDrawable(imageResource);
+        return res;
     }
 }
