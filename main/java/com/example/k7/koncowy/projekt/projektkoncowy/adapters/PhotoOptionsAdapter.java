bd48499318fffe01ed479c11123e7bfc951ce5d4
a <a>
2019-04-23 13:43:08 +0200

    trying to fix photo sharing.

--- main/java/com/example/k7/koncowy/projekt/projektkoncowy/adapters/PhotoOptionsAdapter.java
+++ main/java/com/example/k7/koncowy/projekt/projektkoncowy/adapters/PhotoOptionsAdapter.java
@@ -10,6 +10,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -124,7 +125,7 @@ public class PhotoOptionsAdapter extends ArrayAdapter {
                 else if(position==2)
                 {
                     Intent share = new Intent(Intent.ACTION_SEND);
-                    share.setType("image/*");
+                    share.setType("image/jpeg");
                     SimpleDateFormat df = new SimpleDateFormat("yyyyMMdd_HHmmss");
                     String d = df.format(new Date());
                     String tempFileName = d+"tymczasowy.jpg";
