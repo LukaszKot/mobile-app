de94e75465fbf7a9a86888c552dd132ddf6e0f42
aaa <aaa>
2019-04-27 11:01:11 +0200

    Added network activity.

---
+++ main/java/com/example/k7/koncowy/projekt/projektkoncowy/activities/NetworkActivity.java
@@ -0,0 +1,15 @@
+package com.example.k7.koncowy.projekt.projektkoncowy.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.k7.koncowy.projekt.projektkoncowy.R;
+
+public class NetworkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network);
+    }
+}
